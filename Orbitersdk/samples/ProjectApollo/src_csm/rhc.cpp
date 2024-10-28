f921f45fdc9ccd64c8570f482de354805065b2aa
James Pelster <captainswag101@gmail.com>
2024-10-27 19:58:46 -0700

    Fix CSM RHC proportional rate scaling This previously scaled from 0 to 11.5 degrees, which is wrong. Maximum rate is achieved at 10 degrees of deflection. Minimum rate scaling isn't clear based on the documentation, but for joystick deadband purposes I've set this to 1 degree, the same as the LM.

--- Orbitersdk/samples/ProjectApollo/src_csm/rhc.cpp
+++ Orbitersdk/samples/ProjectApollo/src_csm/rhc.cpp
@@ -27,6 +27,7 @@ See http://nassp.sourceforge.net/license/ for more details.
 #include "nasspsound.h"
 #include "toggleswitch.h"
 #include "rhc.h"
+#include <cmath>
 
 RHC::RHC()
 {
@@ -52,16 +53,29 @@ void RHC::Reset()
 void RHC::Timestep(int* rhc_pos, bool normdc, bool normac, bool dirdcab, bool dirdcredun)
 {
 	//Map to -11.5° to 11.5°
-	for (int i = 0;i < 3;i++)
+	for (int i = 0; i < 3; i++)
 	{
-		deflection.data[i] = ((double)(rhc_pos[i] - 32768))*23.0 / 65536.0;
+		deflection.data[i] = ((double)(rhc_pos[i] - 32768)) * 23.0 / 65536.0;
 	}
 
 
 	//Proportional Rate
 	if (normac)
 	{
-		PropRate = deflection / 11.5;
+		// Proportional rate scales from 1° to 10° deflection.
+		for (int i = 0; i < 3; i++)
+		{
+			if (deflection.data[i] >= 0.0)
+			{
+				PropRate.data[i] = min(1.0, max(0.0, (deflection.data[i] - 1.0) / 9.0));
+			}
+			else
+			{
+				PropRate.data[i] = min(0.0, max(-1.0, (deflection.data[i] + 1.0) / 9.0));
+			}
+		}
+
+		//sprintf(oapiDebugString(), "Proportional Rate: X/Y/Z = %f / %f / %f", PropRate.x, PropRate.y, PropRate.z);
 	}
 	else
 	{
