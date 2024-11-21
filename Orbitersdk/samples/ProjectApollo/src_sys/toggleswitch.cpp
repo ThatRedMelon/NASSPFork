23d3cb07e35d898302e78847b9cba00aed45ba4b
Aeasala <esmacdon@gmail.com>
2024-11-21 16:06:04 -0500

    First-order lowpass filter applied to gauge output, instead of slew-style display.

--- Orbitersdk/samples/ProjectApollo/src_sys/toggleswitch.cpp
+++ Orbitersdk/samples/ProjectApollo/src_sys/toggleswitch.cpp
@@ -3817,7 +3817,11 @@ double MeterSwitch::GetDisplayValue() {
 		double dt = oapiGetSimTime() - lastDrawTime; // oapiGetSimTime() - lastDrawTime;
 		if (dt > 0) {
 			if (fabs(value - displayValue) / dt > (maxValue - minValue) / minMaxTime) {
-				displayValue += ((value - displayValue) / fabs(value - displayValue)) * (maxValue - minValue) / minMaxTime * dt;
+				// discrete time LPF where y[n] = y[n-1]*(1-a) + x[n]*a
+				// assumed that 5tau is minMaxTime, i.e. time to reach 99.3% of a step input.
+				// therefore 1/tau is (5/minMaxTime) for each slice {dt}.
+				double filtConstant = max(min(GAUGE_LPF_SCALAR * dt * 5.0/minMaxTime, 1.0),0.0);
+				displayValue = displayValue*(1-filtConstant) + (value*filtConstant);
 			} else {
 				displayValue = value;
 			}
